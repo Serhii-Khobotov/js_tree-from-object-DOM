51fa706815d1c7342af814455c9c8afbc0b3f45b
Serhii Khobotov <hobbitura@gmail.com>
2024-08-07 11:22:57 +0300

    make build

---
+++ index.1e399616.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in t)if(t.hasOwnProperty(a)){var l=document.createElement("li");l.textContent=a,r.appendChild(l),Object.keys(t[a]).length>0&&e(l,t[a])}n.append(r)}(document.getElementById("tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.1e399616.js.map
